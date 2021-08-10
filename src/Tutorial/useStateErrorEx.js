0db24c1dae9f9fd09b6244f71d932002de3448b8
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-10 17:09:38 +0530

    Checking

---
+++ src/Tutorial/useStateErrorEx.js
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+
+function useStateErrorEx() {
+    return (
+        <div>
+            <h2>Use State Error Example</h2>
+        </div>
+    )
+}
+
+export default useStateErrorEx
