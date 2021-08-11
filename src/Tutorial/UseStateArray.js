c15b5d3a62b3d5dffc7ff7f2e00fd6c2bbea9c46
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-11 09:45:17 +0530

    create a data file

--- src/Tutorial/UseStateArray.js
+++ src/Tutorial/UseStateArray.js
@@ -1,7 +1,8 @@
 import React,{useState} from 'react'
+import {data} from "./data";
 
-
-function UseStateArray(props) {
+function UseStateArray() {
+    const [people,setPeople] = React.useState({}) // another way to call UseState
     return (
         <div>
             <h2>Use State Array is in Action</h2>
---
+++ src/Tutorial/data.js
@@ -0,0 +1,6 @@
+export const data = [
+    {id:1, name:"Tony Stark"},
+    {id:2, name:"Hulk"},
+    {id:3, name:"Thor"},
+    {id:4, name:"Caption America"},
+];
