b05676a75d5d28aaaec2c4934451a5b9b3f74514
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-11 10:13:43 +0530

    Create seprate remove

--- src/Tutorial/UseStateArray.js
+++ src/Tutorial/UseStateArray.js
@@ -3,20 +3,26 @@ import { data } from "./data";
 
 function UseStateArray() {
     const [people, setPeople] = React.useState(data) // another way to call UseState 
+
+    const removeItem=(id) =>{
+        let newPeople = people.filter((person) => person.id!==id);
+        setPeople(newPeople);
+    }
     return (
         <>
             {
-            
-            people.map((person) => {
-                const {id, name} = person;
-                return (
-                    <div key={id} className="item">
-                        <h4>{name}</h4> 
-                   </div>
-                )
-            }
-        )}
-        <button className="btn" onClick={() => setPeople([])}>Clear The Avenger</button>
+
+                people.map((person) => {
+                    const { id, name } = person;
+                    return (
+                        <div key={id} className="item">
+                            <h4>{name}</h4>
+                            <button onClick={() => removeItem(id)}>Remove</button>
+                        </div>
+                    )
+                }
+                )}
+            <button className="btn" onClick={() => setPeople([])}>Clear The Avenger</button>
         </>
     )
 }
