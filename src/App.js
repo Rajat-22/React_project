a09237a454984be404e6bea27e8184f548fd5a5e
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-11 14:59:17 +0530

    creating form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import React from "react";
 // import UseStateArray from "./Tutorial/UseStateArray"
 // import UseStateObject from "./Tutorial/UseStateObject";
 // import UsestateCounter from "./Tutorial/UsestateCounter";
-import UseEffectBasic from "./Tutorial/UseEffectBasic";
+// import UseEffectBasic from "./Tutorial/UseEffectBasic";
+import Forms from "./Tutorial/Forms";
 
 function App() {
   return (
     <div className='container'>
-      <h2>Advance React Js</h2>
-      <UseEffectBasic />
+     
+      <Forms />
     </div>
     
     
---
+++ src/Tutorial/Forms.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+// we want to have two input fields
+// we want to have a button
+// one of the input field should accept username and email
+// we want to accept the data and store it somewhere in a variable
+// initial things will be empty
+
+
+function Forms() {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();  // this is used to prevent the default behaviour of the browser
+    console.log("Hello");   // it will show in the console after submit by the button
+  }
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>  
+        {/* here we use onsubmit which works same as onclick in the button  */}
+          <div className='form-control'>
+            <lable htmlFor="FirstName">Name :</lable>
+            <input type="text" id="firstname" name='firstname'></input>
+          </div>
+
+          <div className='form-control'>
+            <lable htmlFor="email">Email :</lable>
+            <input type="text" id="email" name='email'></input>
+          </div>
+
+          <button type="submit">Submit</button>
+
+        </form>
+      </article>
+    </>
+  )
+}
+
+export default Forms
