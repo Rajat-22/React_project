8aa28dcaa779b8dd45717b50368af69929d61e89
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-11 18:07:17 +0530

    add json file

--- src/Tutorial/LeranJSON.js
+++ src/Tutorial/LeranJSON.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import axios from 'axios'
 
 function LeranJSON() {
+    const handleClick = (b) =>{
+        const promise = axios.post(" http://localhost:4000/users",{
+            name:'Rajat'
+        },{
+            headers:{
+                ['content']
+            }
+        })
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+          <button type ="button" className='btn' onClick={handleClick}>Send User</button>
+          <button type ="button" className='btn' onClick={getUsers}>Get User</button>  
+        </>
     )
 }
 
