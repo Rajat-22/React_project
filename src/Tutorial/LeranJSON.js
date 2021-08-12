a61a69fb066ed9e3769feafdc54a7437ee267e1e
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-12 11:26:07 +0530

    complete get user part

--- src/Tutorial/LeranJSON.js
+++ src/Tutorial/LeranJSON.js
@@ -4,10 +4,10 @@ import axios from 'axios'
 function LeranJSON() {
     const handleClick = (b) => {
         const promise = axios.post("http://localhost:4000/users", {
-            name: "Tony"
+            name: "Tony"  // setting the object we want or create
         }, {
             header: {
-                ['content-type']: 'application/json'
+                ['content-type']: 'application/json'  // this setting the server type
             }
         
     })
@@ -15,15 +15,23 @@ function LeranJSON() {
         console.log(response) // this is used to show response from the user
     })
     promise.catch(error => {
-        console.error(error);
+        console.error(error);  // this is used to console our error if we get
     })
     let c = b;
     console.log(b);
     }
+
+    const getUsers = () =>{
+        axios.get("http://localhost:4000/users").then(users => {
+            console.log(users);
+ {/*  we are getting the data from our json server and then storing it in users and then consoling the array of the object */}
+        })
+    }
     return (
         <>
+        <h2> Title </h2>
             <button type="button" className='btn' onClick={handleClick}>Post User</button>
-           {/* <button type="button" className='btn' onClick={getUsers}>Get User</button>  */}
+         <button type="button" className='btn' onClick={getUsers}>Get User</button> 
         </>
     )
 }
