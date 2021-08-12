5c5cb1663a878337ef164012f13209eaaf68ba83
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-12 12:53:24 +0530

    crete for reducer

--- src/Tutorial/Reducer/index.js
+++ src/Tutorial/Reducer/index.js
@@ -1,12 +1,42 @@
-import React, {useState, useReducer} from 'react'
-import model from './model';
+import React, { useState, useReducer } from 'react'
+import Model from './Model';
+import { data } from "../data";
 
-function index() {
+function Index() {
+    const [name, setName] = useState('');
+    const [people, setPeople] = useState(data);
+    const [showModel, setShowModel] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (name) {
+            setShowModel(true);
+            setPeople([...people, { id: new Date().getTime().toString, name }])
+            setName('')
+        }
+        else {
+            setShowModel(true)
+        }
+    }
+
     return (
         <>
-            <h2>Use Reducer</h2>
+            {showModel && <Model />}
+            <form onSubmit={handleSubmit} className='form'>
+                <div>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                </div>
+                <button type="submit">Add User</button>
+            </form>
+            {people.map((person) => {
+                return <>
+                    <div key={person.id}>
+                        <h4>{person.name}</h4>
+                    </div>
+                </>
+            })}
         </>
     )
-}
 
-export default index
+}
+export default Index
--- src/Tutorial/Reducer/model.js
+++ src/Tutorial/Reducer/model.js
@@ -1,1 +1,12 @@
+import React from 'react'
+
+function model() {
+    return (
+        <>
+         <h2>Model</h2>   
+        </>
+    )
+}
+
+export default model
 
