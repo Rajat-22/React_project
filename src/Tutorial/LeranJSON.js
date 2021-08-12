64d4476153b7204535ceaefb2094b386a5a2bb9c
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-12 10:42:05 +0530

    use promise

--- src/Tutorial/LeranJSON.js
+++ src/Tutorial/LeranJSON.js
@@ -2,19 +2,28 @@ import React from 'react'
 import axios from 'axios'
 
 function LeranJSON() {
-    const handleClick = (b) =>{
-        const promise = axios.post(" http://localhost:4000/users",{
-            name:'Rajat'
-        },{
-            headers:{
-                ['content']
+    const handleClick = (b) => {
+        const promise = axios.post("http://localhost:4000/users", {
+            name: "Tony"
+        }, {
+            header: {
+                ['content-type']: 'application/json'
             }
         })
     }
+    promise.then(response => {
+        console.log(response) // this is used to show response from the user
+    })
+    promise.catch(error => {
+        console.error(error);
+    })
+    let c = b;
+    console.log(b);
+
     return (
         <>
-          <button type ="button" className='btn' onClick={handleClick}>Send User</button>
-          <button type ="button" className='btn' onClick={getUsers}>Get User</button>  
+            <button type="button" className='btn' onClick={handleClick}>Post User</button>
+           {/* <button type="button" className='btn' onClick={getUsers}>Get User</button>  */}
         </>
     )
 }
